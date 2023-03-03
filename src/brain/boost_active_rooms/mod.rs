4502594adf8205403874d9a21a73cb32a080250a
James Hendry <jameshendry05@gmail.com>
2023-03-03 21:46:51 +0000

    Reapply boost when it gets near the end.

--- src/brain/boost_active_rooms/mod.rs
+++ src/brain/boost_active_rooms/mod.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::error::Error;
-use chrono::{DateTime, Utc};
+use std::fmt::{Display, Formatter};
+use chrono::{DateTime, Duration, Utc};
 use log::{debug, info, warn};
 use crate::brain::boost_active_rooms::config::BoostActiveRoomsConfig;
 use crate::brain::python_like::control::devices::Device;
@@ -13,7 +14,31 @@ pub mod config;
 const OUR_SET_POINT_ORIGINATOR: &str = "FollowHeatingBoostActiveRooms";
 
 pub struct AppliedBoosts {
-    room_temps: HashMap<String, (f32, DateTime<Utc>)>,
+    room_temps: HashMap<String, AppliedBoost>,
+}
+
+#[derive(Debug)]
+pub struct AppliedBoost {
+    temp_set: f32,
+    end_time: DateTime<Utc>,
+}
+
+impl AppliedBoost {
+    /// Check that this applied boost is the same as the one currently observed
+    /// on wiser.
+    /// Done by checking end times.
+    pub fn matches_wiser(&self, room: &WiserRoomData) -> bool {
+        if let Some(timeout) = room.get_override_timeout() {
+            return timeout == self.end_time;
+        }
+        false
+    }
+}
+
+impl Display for AppliedBoost {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:.1} ends {}", self.temp_set, self.end_time.to_rfc3339())
+    }
 }
 
 impl AppliedBoosts {
@@ -23,32 +48,16 @@ impl AppliedBoosts {
         }
     }
 
-    pub fn mark_applied(&mut self, room: String, temp: f32, datetime: DateTime<Utc>) {
-        self.room_temps.insert(room, (temp, datetime));
+    pub fn mark_applied(&mut self, room: String, temp_set: f32, end_time: DateTime<Utc>) {
+        self.room_temps.insert(room, AppliedBoost { temp_set, end_time});
     }
 
     pub fn clear_applied(&mut self, room: &str) {
         self.room_temps.remove(room);
     }
 
-    /// Check if we have applied a boost to the room and whether
-    /// the end time matches what we set.
-    pub fn have_we_applied_any_boost_to(&self, room: &WiserRoomData) -> bool {
-        if let Some(room_name) = room.get_name() {
-            if let Some((_we_set, time_set)) = self.room_temps.get(room_name) {
-                return room.get_override_timeout().is_some() && room.get_override_timeout().as_ref().unwrap() == time_set;
-            }
-        }
-        return false;
-    }
-
-    pub fn get_applied_boost_temp(&self, room: &WiserRoomData) -> Option<f32> {
-        if let Some(room_name) = room.get_name() {
-            if let Some((we_set, _time_set)) = self.room_temps.get(room_name) {
-                return Some(*we_set);
-            }
-        }
-        return None;
+    pub fn get_applied_boost(&self, room_name: &str) -> Option<&AppliedBoost> {
+        return self.room_temps.get(room_name);
     }
 }
 
@@ -78,13 +87,14 @@ pub async fn update_boosted_rooms(state: &mut AppliedBoosts, config: &BoostActiv
     for room in wiser_data.get_rooms() {
         let room_name = room.get_name();
         if room_name.is_none() {
+            warn!("Failed to get room name from id: {}", room.get_id());
             continue;
         }
         let room_name = room_name.unwrap();
 
         match room_boosts.remove(room_name) {
             None => {
-                if state.have_we_applied_any_boost_to(room) {
+                if state.get_applied_boost(room_name).is_some() {
                     info!("Cancelling boost in room {}", room_name);
                     wiser.get_wiser_hub().cancel_boost(room.get_id(), OUR_SET_POINT_ORIGINATOR.to_string()).await?;
                 }
@@ -100,7 +110,11 @@ pub async fn update_boosted_rooms(state: &mut AppliedBoosts, config: &BoostActiv
                 }
 
                 // If we've applied a boost, we need to check that its OUR boost if we increase it.
-                if state.have_we_applied_any_boost_to(room) {
+                if let Some(applied_boost) = state.get_applied_boost(room_name) {
+                    if applied_boost.matches_wiser(room) {
+                        debug!("No record of applying boost to {} - not touching it.", room_name);
+                        continue;
+                    }
                     debug!("We have already applied a matching boost to {}", room_name);
                     let temp = match room.get_override_set_point() {
                         None => {
@@ -109,20 +123,20 @@ pub async fn update_boosted_rooms(state: &mut AppliedBoosts, config: &BoostActiv
                         }
                         Some(temp) => temp,
                     };
-                    let we_applied_temp = state.get_applied_boost_temp(room);
-                    match we_applied_temp {
-                        None => warn!("Apparently we didn't apply any temp as it turns out...?"),
-                        Some(we_applied_temp) => {
-                            debug!("Current boosted temp {:.1}, we applied {}", temp, we_applied_temp);
-                            if (should_set_to - temp).abs() > 0.3 {
-                                info!("Significant difference between what we applied and what we should be applying now, increasing.");
-                                apply_boost(room, should_set_to, room_name, &device, state, wiser.get_wiser_hub()).await?
-                            }
-                        }
+
+                    debug!("Current boosted temp {:.1}, we applied {}", temp, applied_boost);
+                    if (should_set_to - temp).abs() > 0.3 {
+                        info!("Significant difference between what we applied and what we should be applying now, increasing.");
+                        apply_boost(room, should_set_to, room_name, &device, state, wiser.get_wiser_hub()).await?;
+                        continue;
+                    }
+                    if applied_boost.end_time < Utc::now() - Duration::seconds(2*60) {
+                        info!("Less than two minutes remaining on boost for room {}. Reapplying now.", room_name);
+                        apply_boost(room, should_set_to, room_name, &device, state, wiser.get_wiser_hub()).await?;
+                        continue;
                     }
                     continue;
                 }
-                debug!("No record of applying boost to {} - not touching it.", room_name);
             }
         }
     }
